fc47ff848c26be5137a21d31c64d86ad5e92e9cb
Mounir24 <M.brtouli997@gmail.com>
2021-11-18 19:32:33 +0100

    BASE URL UPDATED

--- public/JS/chart_data.js
+++ public/JS/chart_data.js
@@ -1,7 +1,7 @@
 /*---- GRAPHICING DATA --> CHART.JS LIBRARY ----*/
 // API FOR GRAPHCING DATA USAGE
 // BASE URL: https://short-url-snaplink.herokuapp.com (ORIGINAL)
-const BASE_URL1 = 'http://localhost:4041';
+const BASE_URL1 = 'https://short-url-snaplink.herokuapp.com/';
 
 (async function () {
     // FETCH DATA FROM API 
