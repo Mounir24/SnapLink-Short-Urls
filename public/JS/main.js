3b883e394584b92777d242613ad37e3198b5be6d
Mounir24 <M.brtouli997@gmail.com>
2021-12-08 14:31:05 +0100

    Updated Redirect Page

--- public/JS/main.js
+++ public/JS/main.js
@@ -382,24 +382,3 @@ $('#url_form').submit(e => {
 /*document.addEventListener('contextmenu', (e) => {
     e.preventDefault()
 })*/
-
-
-
-
-
-// REDIRECT SCRIPT HELPER 
-
-// REDIRECT COUNTER 
-
-/*let count = 10;
-
-function counter() {
-    let counter = document.querySelector('.redirect__count');
-    count--;
-    counter.innerText = count;
-    if (count <= 0) {
-        clearInterval(stop);
-        window.location = "<%= url %>";
-    }
-}
-const stop = setInterval(counter, 1000);*/
